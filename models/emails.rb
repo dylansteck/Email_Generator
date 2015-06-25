a6aaa3e1d30c4c25f584873b7ee4cb99e578337a
Dylan Steck <dylancsteck@gmail.com>
2015-06-25 18:38:18 +0000

    update from the end of the day, MVP is done

--- models/emails.rb
+++
@@ -1,8 +0,0 @@
-class Emails
-attr_accessor :type, :message
-	def initialize (type, message)
-		@type = type
-		@message = message
-	end
-		
-end
--- models/messages.rb
+++ models/messages.rb
@@ -1,6 +1,11 @@
-class Messages
-	attr_accessor :email_types
-	def initialize
-		@email_types= ["Apology Email", "Thank You Email", "Date Proposal Email", "Hello to an Old Friend Email", "Homework Excuse Email", "Extention Email for a Project or Due Date", "Happy Birthday Email"]
+class Emails
+	attr_accessor :message
+	def initialize(message)
+		@message = message
 	end
+	
+	
+	
+		
 end
+
