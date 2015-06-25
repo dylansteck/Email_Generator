eb5aaafb550303c2928d64309e98f56dae1dfb86
Dylan Steck <dylancsteck@gmail.com>
2015-06-25 13:28:35 +0000

    update message

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,12 @@
 require 'bundler'
 Bundler.require
-require_relative "/models/emails.rb"
+# require_relative "/models/emails.rb"
 class MyApp < Sinatra::Base
 	
 	get "/" do
 		erb :index
 	end
+	get '/begin.erb' do
+		erb:begin
+  end
 end
